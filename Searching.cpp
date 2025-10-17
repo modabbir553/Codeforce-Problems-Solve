54342e7d83abc17f7ba5aa0e5336dffe5ab20048
modabbir553 <modabbir553@gmail.com>
2025-10-17 07:01:10 +0600

    solved a new prblm

--- Searching.cpp
+++ Searching.cpp
@@ -5,6 +5,23 @@ int main() {
 ios_base::sync_with_stdio(false);
 cin.tie(0);
 cout.tie(0);
-    // Code like pro
+    int n, x;
+    cin >> n;
+    int ar[n];
+
+    for(int i =0; i<n; i++){
+        cin >> ar[i];
+    }
+
+    cin >> x;
+
+    for(int j = 0; j<n; j++){
+        if(ar[j] == x){
+            cout << j;
+            return 0;
+        }
+    }
+    cout << -1;
+
     return 0;
 }
