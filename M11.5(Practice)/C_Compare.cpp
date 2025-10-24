3612f577b035f71a31cd87bc50f57c58b6fddd0d
modabbir553 <modabbir553@gmail.com>
2025-10-24 19:37:46 +0600

    solved a new prblm

--- M11.5(Practice)/C_Compare.cpp
+++ M11.5(Practice)/C_Compare.cpp
@@ -2,80 +2,25 @@
 #include <cstring>
 using namespace std;
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    char str1[20];
-    char str2[20];
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+    char A[100], B[100];
+    cin >> A >> B;
 
-    cin >> str1 >> str2;
-    int i = 0;
-
-    while (1)
+    int value = strcmp(A, B);
+    if (value < 0)
     {
-        if (str1[i] == '\0' && str2[i] == '\0')
-        {
-            cout << str1;
-            break;
-        }
-        else if (str1[i] == '\0')
-        {
-            cout << str1;
-            break;
-        }
-        else if (str2[i] == '\0')
-        {
-            cout << str2;
-            break;
-        }
-        else if (str1[i] == str2[i])
-        {
-            i++;
-        }
-        else if (str1[i] < str2[i])
-        {
-            cout << str1;
-            break;
-        }
-        else
-        {
-            cout << str2;
-            break;
-        }
+        cout << A << endl;
     }
-
+    else if (value > 0)
+    {
+        cout << B << endl;
+    }
+    else
+    {
+        cout << A << endl;
+    }
     return 0;
 }
-
-// #include <iostream>
-// #include <cstring>
-// using namespace std;
-
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(0);
-//     cout.tie(0);
-//     char str1[20];
-//     char str2[20];
-
-//     cin >> str1 >> str2;
-
-//     int v = strcmp(str1, str2);
-//     if (v < 0)
-//     {
-//         cout << str1;
-//     }
-//     else if (v > 0)
-//     {
-//         cout << str2;
-//     }
-//     else
-//     {
-//         cout << str1;
-//     }
-
-//     return 0;
-// }
