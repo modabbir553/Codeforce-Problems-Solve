764c15f3be1e1e442120931b196969c0a267abe6
modabbir553 <modabbir553@gmail.com>
2025-10-24 19:10:31 +0600

    done some changes

--- M11.5(Practice)/J_Count_Letters.cpp
+++ M11.5(Practice)/J_Count_Letters.cpp
@@ -6,26 +6,25 @@ int main() {
 ios_base::sync_with_stdio(false);
 cin.tie(0);
 cout.tie(0);
-    char str[10000001];
-    cin >> str;
+    char str1[10000001];
+    cin >> str1;
 
-    int l = strlen(str);
+    int l = strlen(str1);
     int fre[26] = {0};
     char alpha[27];
     int v;
 
     for (int i = 0; i < l; i++)
     {
-        v = (int)str[i] - 97;
+        v = (int)str1[i] - 97;
         fre[v] += 1;
-        alpha[v] = str[i];
+        alpha[v] = str1[i];
     }
 
     for (int j = 0; j < 26; j++)
     {
         if (fre[j] != 0)
         {
-            // printf("%c : %d\n", alpha[j], fre[j]);
             cout << alpha[j] << " : " << fre[j] << endl;
         }
     }
