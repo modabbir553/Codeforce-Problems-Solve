4674bfd70b2fddbae53caad35c1075506186b814
modabbir553 <modabbir553@gmail.com>
2025-10-24 19:06:50 +0600

    done some changes

--- M11.5(Practice)/J_Count_Letters.cpp
+++ M11.5(Practice)/J_Count_Letters.cpp
@@ -1,10 +1,13 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <cstring>
+using namespace std;
 
-int main()
-{
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
     char str[10000001];
-    scanf("%s", &str);
+    cin >> str;
 
     int l = strlen(str);
     int fre[26] = {0};
@@ -22,7 +25,8 @@ int main()
     {
         if (fre[j] != 0)
         {
-            printf("%c : %d\n", alpha[j], fre[j]);
+            // printf("%c : %d\n", alpha[j], fre[j]);
+            cout << alpha[j] << " : " << fre[j] << endl;
         }
     }
     return 0;
